d41c01863ce8ac6d24b6d12135a90ebe40330b57
Michelle-Minakov <teresukm339@gmail.com>
2026-01-30 11:51:45 +0200

    тестова попитка Терещук

---
+++ examples/run-all.ts
@@ -0,0 +1,7 @@
+console.log("=== Anti-Examples ===");
+
+import { runFactoryMethodAntiExample } from "./anti-patterns/factory-method";
+import { runDecoratorAntiExample } from "./anti-patterns/decorator";
+
+runFactoryMethodAntiExample();
+runDecoratorAntiExample();
